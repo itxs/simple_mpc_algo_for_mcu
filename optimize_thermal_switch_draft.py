3e9379ef2a09ec15f03766df7eba019cecb285c7
Timur <timuritxs@gmail.com>
2024-06-30 11:56:07 +0200

    Some improvements

--- optimize_thermal_switch_draft.py
+++ optimize_thermal_switch_draft.py
@@ -2,28 +2,22 @@ import matplotlib.pyplot as plt
 import numpy as np
 from matplotlib.widgets import Slider
 
-## Time properties
-T_END = 100  # seconds
-DT = 0.01  # seconds
-
-## Value properties
-RTOL = 1e-3
-
-TEMP_MAX = 120  # degrees
-PWR_MAX = 500  # Watts
+T_END = 100  # Max prediction time in seconds
+DT = 0.01  # Integration step duration in seconds
+ABSTOL = 0.25  # Absolute tolerance in degrees
+TEMP_MAX = 120  # Max temperature in degrees
+PWR_MAX = 500  # Max power in watts
 
 a = np.array([[0, 1], [1, 1]], dtype=float)
 b = np.array([[0], [1]], dtype=float)
-
-
 t = np.linspace(0, T_END, int(T_END / DT))
 u = np.zeros(len(t))
 y = np.zeros(len(t))
-tau = 20
-k = 100
-t1 = 60
-delay = 6  # seconds
-t0 = 25  # degrees
+tau = 20  # system inertia
+k = 100  # power in watts
+t1 = 60  # set point temperature in degrees
+delay = 6  # pure delay in seconds
+t0 = 25  # initial temperature in degrees
 
 fig, ax = plt.subplots()
 ax.set_xlim(0, T_END)
@@ -47,14 +41,14 @@ def ss_step(x, u, dt):
 
 def binary_search(f, left: float, right: float, tolerance: float):
     while left < right:
-        midpoint = (left + right) / 2.0
+        midpoint = (left + right) / 2
         f_mid = f(midpoint)
         if abs(f_mid) < tolerance:
             return midpoint
         elif f_mid > 0:
-            right = midpoint - tolerance / 2
+            right = midpoint - DT
         else:
-            left = midpoint + tolerance / 2
+            left = midpoint + DT
     return right
 
 
@@ -106,24 +100,26 @@ def simulate(tSwitch: float):
 def update(val=0):
     global delay, t0, t1
     # Take settings from user controls
-    a[1][0] = -1 / (tauSlider.val * tauSlider.val)
-    a[1][1] = -(tauSlider.val + tauSlider.val) / (tauSlider.val * tauSlider.val)
-    b[1] = kSlider.val / (tauSlider.val * tauSlider.val)
+    tau = tauSlider.val
+    tau_2 = tau * tau
+    a[1][0] = -1 / tau_2
+    a[1][1] = -(tau + tau) / tau_2
+    b[1] = kSlider.val / tau_2
     t0 = t0Slider.val
-    t1 = t1Slider.val - t0
+    t1 = t1Slider.val
     delay = delaySlider.val
 
-    if t1 > 0:
+    if t1 > t0:
         # Search for optimal value using binary search algorithm
-        t_switch = binary_search(lambda x: simulate(x)[0][-1] - t1, 0, T_END, RTOL)
+        t_switch = binary_search(lambda x: simulate(x)[0][-1] - (t1 - t0), 0, T_END, ABSTOL)
     else:
         t_switch = 0.0
 
     # Simulate found optimal solution curve and redraw all
     y, imax = simulate(t_switch)
     endline.set_xdata([imax])
-    setline.set_ydata([t1 + t0])
-    curves[0].set_ydata(y + np.ones(len(y)) * t0)
+    setline.set_ydata([t1 - t0])
+    curves[0].set_ydata(y)
     curves[0].set_xdata(t[: len(y)])
     curves[1].set_ydata(np.multiply(u, 100))
     curves[1].set_xdata(t)
@@ -132,11 +128,11 @@ def update(val=0):
     fig.canvas.draw_idle()
 
 
-delaySlPos = plt.axes([0.1, 0.16, 0.82, 0.05], facecolor="lightgoldenrodyellow")
-tauSlPos = plt.axes([0.1, 0.12, 0.82, 0.05], facecolor="lightgoldenrodyellow")
-t1SlPos = plt.axes([0.1, 0.08, 0.82, 0.05], facecolor="lightgoldenrodyellow")
-t0SlPos = plt.axes([0.1, 0.04, 0.82, 0.05], facecolor="lightgoldenrodyellow")
-kSlPos = plt.axes([0.1, 0, 0.82, 0.05], facecolor="lightgoldenrodyellow")
+delaySlPos = plt.axes([0.1, 0.18, 0.82, 0.02], facecolor="lightgoldenrodyellow")
+tauSlPos = plt.axes([0.1, 0.14, 0.82, 0.02], facecolor="lightgoldenrodyellow")
+t1SlPos = plt.axes([0.1, 0.1, 0.82, 0.02], facecolor="lightgoldenrodyellow")
+t0SlPos = plt.axes([0.1, 0.06, 0.82, 0.02], facecolor="lightgoldenrodyellow")
+kSlPos = plt.axes([0.1, 0.02, 0.82, 0.02], facecolor="lightgoldenrodyellow")
 
 delaySlider = Slider(ax=delaySlPos, label="τd", valmin=DT * 2, valmax=40, valinit=delay)
 tauSlider = Slider(ax=tauSlPos, label="τ1", valmin=DT * 2, valmax=40, valinit=tau)
